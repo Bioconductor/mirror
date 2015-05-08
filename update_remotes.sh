65025ad7f3a5ae70579e17a778d237e90b7c97c2
Jim Hester <james.f.hester@gmail.com>
2015-05-08 13:14:36 -0400

    Remote script now sets things up properly
    Works for both mirror clones and existing repositories.

--- update_remotes.sh
+++ update_remotes.sh
@@ -7,22 +7,66 @@
 set -eou pipefail
 IFS=$'\n\t'
 
-package=$(git remote -v | perl -ne 'if (m!/([^/]+?)(?:.git)?\s!) { print $1; exit}')
-git checkout master
-git svn init "https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/$package"
-git update-ref refs/remotes/git-svn refs/remotes/origin/master
-git svn rebase
+base_url="https://hedgehog.fhcrc.org/bioconductor/"
 
-release_branches=$(git branch -r | perl -ne 'if (m!origin/(release-.*)!) { print $1, "\n" }')
-for release_branch in $release_branches; do
-  svn_branch=$(echo $release_branch | perl -ne 'if (/release-(\d+)\.(\d+)/) { print "RELEASE_$1_$2"; }')
-  svn_url="https://hedgehog.fhcrc.org/bioconductor/branches/$svn_branch/madman/Rpacks/$package"
-  git config --add svn-remote.$release_branch.url $svn_url
-  git config --add svn-remote.$release_branch.fetch :refs/remotes/git-svn-$release_branch
-  git branch --track $release_branch origin/$release_branch
-  git update-ref refs/remotes/git-svn-$release_branch refs/heads/$release_branch
-done
+is_mirror_clone () {
+  git remote show origin | grep -i -q bioconductor-mirror
+  return $?
+}
 
-git remote add bioc https://github.com/Bioconductor-mirror/${package}.git
+add_release_tracking () {
+  local branch=$1
+  shift
+  local remote=$1
+  shift
+  for release_branch in $@; do
+    svn_branch=$(echo $release_branch | perl -ne 'if (/release-(\d+)\.(\d+)/) { print "RELEASE_$1_$2"; }')
+    svn_url="$base_url/branches/$svn_branch/madman/Rpacks/$package"
+    git config --add svn-remote.$release_branch.url $svn_url
+    git config --add svn-remote.$release_branch.fetch :refs/remotes/git-svn-$release_branch
+    git update-ref refs/remotes/git-svn-$release_branch refs/$remote/$release_branch
+  done
+}
 
-echo "Commit to git as normal, use git pull bioc to sync, to push your commits to svn, use git svn dcommit"
+if is_mirror_clone; then
+    # Bioc-mirror clone
+    package=$(git remote -v | perl -ne 'if (m!/([^/]+?)(?:.git)?\s!) { print $1; exit}')
+    git checkout master
+    git svn init "$base_url/trunk/madman/Rpacks/$package"
+    git update-ref refs/remotes/git-svn refs/remotes/origin/master
+    git svn rebase
+    git remote add bioc https://github.com/Bioconductor-mirror/${package}.git
+
+    release_branches=$(git branch -r | perl -ne 'if (m!origin/(release-.*)!) { print $1, "\n" }')
+    add_release_tracking origin heads $release_branches
+
+    for release_branch in $@; do
+      git branch --track $release_branch $branch/$release_branch
+    done
+
+    cat <<\END
+Commit to git as normal, when you want to push your commits to svn
+  1. `git svn rebase` to sync with the latest resuts
+  2. `git svn dcommit` to commit your changes
+END
+
+else
+    # existing repo clone
+    package=$(git remote -v | perl -ne 'if (m!/([^/]+?)(?:.git)?\s!) { print $1; exit}')
+    git remote add bioc "https://github.com/Bioconductor-mirror/${package}.git"
+    git fetch bioc
+    git config --add svn-remote.devel.url "$base_url/trunk/madman/Rpacks/$package"
+    git config --add svn-remote.devel.fetch :refs/remotes/git-svn-devel
+    git update-ref refs/remotes/git-svn-devel refs/remotes/bioc/master
+
+    release_branches=$(git branch -r | perl -ne 'if (m!bioc/(release-.*)!) { print $1, "\n" }')
+    add_release_tracking bioc remotes/bioc $release_branches
+    cat <<\END
+Commit to git as normal, when you want to push your commits to svn
+  1. `git checkout bioc/master` to switch to the Bioconductor mirror.
+  2. `git pull --rebase` to update to the latest mirror state.
+  3. `git merge master` to merge your changes into the current svn state.
+  4. `git svn rebase` to sync with the latest svn state.
+  5. `git svn dcommit` to commit your changes
+END
+fi
